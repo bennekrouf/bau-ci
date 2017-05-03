4cfc41b030343d0704f970d5af941f25883dece5
Antonio Rodrigues <antonio.rodrigues@b-i.com>
2017-05-03 11:03:44 +0100

    Add test methods

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 import { expect as expect } from 'chai'
-import ttt from '../src/module.js'
+import BauCI from '../src/module.js'
 
 describe('bau-ci', () => {
   it('should be runing without any problems', () => {
-    expect(ttt).to.not.throw()
+    expect(BauCI).to.not.throw()
   })
 })
